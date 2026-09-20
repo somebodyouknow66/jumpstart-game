8b83ee5705ada31d95dd72c745f80acf9007904c
Sakhi Akram <sakhiakram.github@outlook.com>
2026-09-20 16:34:06 +0500

    enemy functionality
    well, in godot i forget that i have to make commits, i just got out of a super crazy error and i am very glad.

--- enemy.gd
+++ enemy.gd
@@ -1,10 +1,14 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+var speed = 150
 const JUMP_VELOCITY = -400.0
+@onready var enemy: AnimatedSprite2D = $AnimatedSprite2D
+@onready var player: Area2D = $"../Player/playerarea"
+@onready var collision_shape: CollisionShape2D = $"../Player/CollisionShape2D"
 
-@onready var player: CharacterBody2D = $"../Player"
+
+
 
 
 func _physics_process(delta: float) -> void:
@@ -12,9 +16,17 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	var speed: float = 0.01 # put wanted speed here
+	
+	var distance_x := player.global_position.x - global_position.x
+	velocity.x = clampf(distance_x / delta, -speed, speed)
+	enemy.play("run")
+	
+	if distance_x > 0:
+		enemy.flip_h = true
+	elif distance_x < 0: 
+		enemy.flip_h = false
 
-	look_at(player.global_position)
-	self.position = global_position.lerp(player.global_position, speed)
-
+	
+		
+		
 	move_and_slide()
